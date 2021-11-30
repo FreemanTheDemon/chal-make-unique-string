e506e4214685e76343d9deaff6286e4e6fb18d56
Freeman Bird <freemanbird@gmail.com>
2021-11-30 09:40:51 -0700

    challenge complete

--- makeUnique.js
+++ makeUnique.js
@@ -1,1 +1,17 @@
-// Write your solution below:
+const makeUnique = (str) => {
+    let arr = str.split('');
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = (i + 1); j < arr.length; j++) {
+            if (arr[j] === arr[i]) {
+                arr.splice(j, 1);
+            }
+        }
+    }
+    return arr.join('');
+}
+
+console.log(makeUnique('helloworld'))
+// helowrd
+
+console.log(makeUnique('iwanttoclimbamountain'))
+// iwantoclmbu
